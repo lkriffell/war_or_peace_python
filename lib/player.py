1bd03df6a8ab18f76c2323937b5cc9195e4d748b
Logan Riffell <lkriffell@gmail.com>
2021-01-22 15:11:58 -0700

    Add player class and test it exists

---
+++ lib/player.py
@@ -0,0 +1,10 @@
+class Player:
+  def __init__(self, name, deck):
+    self.name = name
+    self.deck = deck
+
+  def has_lost(self):
+    if self.deck.cards == []:
+      return True
+    else:
+      return False
