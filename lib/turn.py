7665f7d12e963299137c58c3639aa1c5bfdae48c
Logan Riffell <lkriffell@gmail.com>
2021-01-26 13:15:15 -0700

    Add War class and add checks to turn class methods

--- lib/turn.py
+++ lib/turn.py
@@ -7,38 +7,45 @@ class Turn:
     self.spoils = []
 
   def turn_type(self):
-    if self.player1.deck.rank_of_card_at(0) == self.player2.deck.rank_of_card_at(0) and self.player1.deck.rank_of_card_at(2) == self.player2.deck.rank_of_card_at(2):
-      return 'MAD!'
-    elif self.player1.deck.rank_of_card_at(0) == self.player2.deck.rank_of_card_at(0):
-      return 'War!'
-    else:
+    if self.player1.deck.value_of_card_at(0) != self.player2.deck.value_of_card_at(0):
       return 'Basic'
+    elif self.player1.deck.value_of_card_at(0) == self.player2.deck.value_of_card_at(0) and len(self.player1.deck.cards) >= 3 and len(self.player2.deck.cards) >= 3 and self.player1.deck.value_of_card_at(2) != self.player2.deck.value_of_card_at(2):
+      return 'War!'
+    elif len(self.player1.deck.cards) > 1 and len(self.player2.deck.cards) > 1:
+      if self.player1.deck.value_of_card_at(0) == self.player2.deck.value_of_card_at(0) and self.player1.deck.value_of_card_at(2) == self.player2.deck.value_of_card_at(2):
+        return 'MAD!'
 
   def winner(self):
     if self.turn_type() == 'Basic':
-      if self.player1.deck.rank_of_card_at(0) > self.player2.deck.rank_of_card_at(0):
+      if self.player1.deck.value_of_card_at(0) > self.player2.deck.value_of_card_at(0):
         return self.player1
-      elif self.player1.deck.rank_of_card_at(0) < self.player2.deck.rank_of_card_at(0):
+      elif self.player1.deck.value_of_card_at(0) < self.player2.deck.value_of_card_at(0):
         return self.player2
     elif self.turn_type() == 'War!':
-      if self.player1.deck.rank_of_card_at(2) > self.player2.deck.rank_of_card_at(2):
+      if len(self.player1.deck.cards) == 1:
+        return self.player2
+      elif len(self.player2.deck.cards) == 1:
         return self.player1
-      elif self.player1.deck.rank_of_card_at(2) < self.player2.deck.rank_of_card_at(2):
+      elif self.player1.deck.value_of_card_at(2) > self.player2.deck.value_of_card_at(2):
+        return self.player1
+      elif self.player1.deck.value_of_card_at(2) < self.player2.deck.value_of_card_at(2):
         return self.player2
     else:
       self.spoils = []
       return 'No Winner'
 
   def pile_those_cards(self, p1_cards, p2_cards):
-    if turn_type() == 'Basic':
+    if self.turn_type() == 'Basic':
       num_cards = 1
     else:
       num_cards = 2
     for i in range(num_cards):
-      self.spoils.append(p1_cards[0])
-      self.player1.deck.remove_card()
-      self.spoils.append(p2_cards[0])
-      self.player2.deck.remove_card()
+      if len(p1_cards) > 0:
+        self.spoils.append(p1_cards[0])
+        self.player1.deck.remove_card()
+      if len(p2_cards) > 0:
+        self.spoils.append(p2_cards[0])
+        self.player2.deck.remove_card()
 
   def award_spoils(self):
     if type(self.winner()) == Player:
---
+++ war.py
@@ -0,0 +1,58 @@
+from lib.card_generator import CardGenerator
+from lib.player import Player
+from lib.turn import Turn
+from lib.deck import Deck
+
+class War:
+  def __init__(self):
+    self.player1 = ''
+    self.player2 = ''
+    self.turn_count = 0
+    self.create_players()
+
+  def create_players(self):
+    generator = CardGenerator()
+    self.player1 = Player("Darrel", Deck(generator.deck1))
+    self.player2 = Player("Phillis", Deck(generator.deck2))
+
+  def new_turn(self):
+    self.turn_count += 1
+    return Turn(self.player1, self.player2)
+  
+  def winner_of_game(self):
+    if len(self.player1.deck.cards) == 0:
+      return self.player2
+    elif len(self.player2.deck.cards) == 0:
+      return self.player1
+
+  def start(self):
+    print('Welcome to War! (or Peace) This game will be played with 52 cards.')
+    print('The players today are %s and %s.'%(self.player1.name, self.player2.name))
+    go = input("Type 'GO' to start the game!")
+    if go.lower() == 'go':
+      while self.player1.has_lost() == False and self.player2.has_lost() == False and self.turn_count < 1000000:
+        turn = self.new_turn()
+        winner = turn.winner()
+        print(self.player1.has_lost())
+        print(self.player2.has_lost())
+        if turn.turn_type() == 'Basic':
+          turn.pile_those_cards(self.player1.deck.cards, self.player2.deck.cards)
+          turn.award_spoils()
+          print("turn %d: %s won 2 cards!"%(self.turn_count, winner.name))
+        elif turn.turn_type() == 'War!':
+          if winner == None:
+            import ipdb; ipdb.set_trace()
+          turn.pile_those_cards(self.player1.deck.cards, self.player2.deck.cards)
+          turn.award_spoils()
+          print("turn %d: War! %s won 6 cards!"%(self.turn_count, winner.name))
+        elif winner == 'No Winner':
+          turn.pile_those_cards(self.player1.deck.cards, self.player2.deck.cards)
+          print("turn %d: *Mutaully Assured Destruction* 6 cards removed from play"%(self.turn_count))
+      if self.turn_count == 1000000:
+        print('---- DRAW ----')
+      else:
+        winner = self.winner_of_game()
+        print("And the winner is: %s!"%(winner.name))
+    else:
+      print('ok dont play...')
+    
