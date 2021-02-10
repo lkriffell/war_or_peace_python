fca39fdd471d242557135dd9ab1d95170b7b97fe
Logan Riffell <lkriffell@gmail.com>
2021-02-10 16:03:59 -0700

    Update test

--- test/war_test.py
+++ test/war_test.py
@@ -1,15 +1,17 @@
+from lib.war import War
 import unittest
 import sys
 sys.path.append('.')
 
 from contextlib import contextmanager
-from war import War
+from lib.war import War
 from lib.player import Player
 
 class TestWar(unittest.TestCase):
   def test_it_exists_and_can_be_won(self):
     self.war = War()
     assert type(self.war) == War
+    self.war.start()
     assert type(self.war.winner_of_game()) == Player
 
 
